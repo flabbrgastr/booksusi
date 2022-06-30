bd654462ce6cfed3536828f1b95493c1d8ed834e
flabbrgastr <flabbrgastr@users.noreply.github.com>
2022-06-30 23:33:48 +0200

    cleanup

--- getallbooksusi.sh
+++ getallbooksusi.sh
@@ -15,43 +15,43 @@ user=$(whoami)
 
 # wget args
 arg1="-e robots=off"
-arg2="-k -K --adjust-extension -I /files"
+arg2="-q -k -K --adjust-extension -I /files"
 arg3="-U mozilla"
 arg4="-p -nH -nd"
 arg5="--convert-links --random-wait"
-
 args="${arg1} ${arg2} ${arg3} ${arg4} ${arg5}"
-echo "$args"
 
 declare -a html1arr=("analsex" "anal_natur_no_condom" "gesichtsbesamung_cum_on_face" "mundvollendung_cum_in_mouth")
 declare -a html_pages=(6 3 7 9)
-#declare -a html_pages=(1 1 1 1)
+#declare -a html_pages=(1 1 1 1)  #for testing
 
 datum=$(date +%Y-%m-%d_%H%M%S)
-
-# html="https://booksusi.com/service/analsex/?&city=wien&service=2&page="
 html0="https://booksusi.com/service/"
 html2="/?&city=wien&page="
 
+echo "Getallbooksusi Page Downloading Script"
+echo "Downloading [ ${html1arr[@]} ] into ./data/$datum..."
+
 #for i in "${html1arr[@]}"
 for i in 0 1 2 3; do
-#   echo "$html0${html1arr[i]}$html2"	#url name
-#   echo "${html_pages[i]}" 
-   echo "##.................................................##"
-   echo "###   ${html1arr[i]}   ${html_pages[i]} pages"
-   echo "##.................................................##"
-a
-   out_dir=./data/booksusi_${html1arr[i]}_$datum
+   echo -n "${html1arr[i]} ${html_pages[i]}"
+   out_dir=./data/$datum
    arg_out=" -P"${out_dir}"/"
-   # or do whatever with individual element of the array
    x=1
    while [ $x -le ${html_pages[i]} ]; do
      wget ${args}$arg_out $html0${html1arr[i]}$html2$x
-     mv ${out_dir}"/"index*$x.html ${out_dir}"/"index_${html1arr[i]}$x.html
-     rm ${out_dir}"/"index*$x.html
+     echo -n "."
+     mv ${out_dir}"/"index*$x.html ${out_dir}"/"${html1arr[i]}$x.html
      x=$(( $x + 1 ))
    done
+   echo
 done
-# cleaning up
-rm ${out_dir}"/*.orig"
-rm ${out_dir}"/banner*.jpg"
+cd $out_dir
+echo "cleaning up..."
+rm *.orig
+rm *.jpg*
+#rm -v !"*.html"
+echo "Contents of $(pwd):"
+ls
+#ls -1 | sed -e 's/\..*$//'
+echo "finished, enjoy!"
