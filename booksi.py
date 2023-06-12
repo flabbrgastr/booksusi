cbf101ff766c20940c6c565156e2ad29b2c2f8f8
fg@amd <flaggrgastr@gmail.com>
2023-06-12 17:55:56 +0200

    generating csv and sortable html table

--- booksi.py
+++ booksi.py
@@ -1,50 +1,48 @@
-import gallib
+import gallib as gl
 import pandas as pd
 import os
 
 # Specify the directory
 dir_path = './data'  # replace with your directory
 
-
 # leave only one, ie the newest, zip file for each date
-dfiles = gallib.clean_files(dir_path, test_mode=False)
-if dfiles:
-    print(str(dfiles) + 'files cleaned')
+dfiles = gl.clean_files(dir_path, test_mode=False)
+ddirs = gl.clean_dirs(dir_path, test_mode=False)
+if dfiles or ddirs:
+    print(str(dfiles) +' / '+str(dfiles) + '   dirs / files cleaned')
 
 # get the last directory
-lastdir = gallib.getlastdir(dir_path)
+lastdir = gl.getlastdir(dir_path)
 
 # get the names of categories in the last directory
 # summarize each category into a single file
-names = gallib.ex_names(lastdir)
+names = gl.ex_names(lastdir)
 if names:
     print('entering '+lastdir)
     for name in names:
-        catted = gallib.cat_files(lastdir, name)
+        catted = gl.cat_files(lastdir, name)
         print('    '+name + ' ... ' + str(catted))
 else:
     print('no files found in ' + lastdir)
 
 # extract gals out of each category file
 # and save them in a separate csv file
-
-print('⌵ '+lastdir[2:])
-
-print('⌵ getgals')
-html_files = gallib.findhtmls(lastdir)
+print('⌵ getgals '+lastdir[2:])
+html_files = gl.findhtmls(lastdir)
 #dataframes = []
 pdall = pd.DataFrame()
 
 for file in html_files:
     category = os.path.splitext(file)[0]
 
-    arr = gallib.get_gals(lastdir, file)
+    arr = gl.get_gals(lastdir, file)
     df = pd.DataFrame(arr)
+ #   print(df.head(3))
     pdall = pd.concat([pdall, df], ignore_index=True)
 
-pdall = gallib.dfComprehend(pdall)
+pdall = gl.dfComprehend(pdall)
+gl.someStats(pdall)
 
-gallib.someStats(pdall)
 
 # Create the directory if it doesn't exist
 if not os.path.exists(lastdir+'/gen/'):
@@ -54,13 +52,10 @@ if not os.path.exists(lastdir+'/gen/'):
 csv_file = lastdir+'/gen/'+'all.csv'
 pdall.to_csv(csv_file, index=False, mode='w')
 
-
-'''    
-    csv_file = lastdir+'/gen/'+category+'.csv'
-    df.to_csv(csv_file, index=False, mode='w')
-    # Write DataFrame to HTML table (overwrite if exists)
-    html_table = df.to_html(index=False)
-    html_file = lastdir+'/gen/'+category+'.html'
-    with open(html_file, 'w') as hfile:
-        hfile.write(html_table)
-'''
+print('⌵ write csv and html')
+# pdall = pd.read_csv(lastdir+'/gen/all.csv')
+html_table = gl.convert_dataframe_to_html(pdall)
+#html_table = gallib.df_to_html2(pdall)
+html_file = lastdir+'/gen/'+'all.html'
+with open(html_file, 'w') as hfile:
+    hfile.write(html_table)
--- gallib.py
+++ gallib.py
@@ -5,11 +5,13 @@ import glob
 from collections import defaultdict
 import pandas as pd
 from bs4 import BeautifulSoup as soup  # HTML data structure
+import shutil
+
 
 def clean_files(dir_path, test_mode=False):
     # Get list of files in the directory
     files = glob.glob(os.path.join(dir_path, '*.tar.gz'))
-    dfiles=0
+    dfiles = 0
     # Group files by date
     files_by_date = defaultdict(list)
     for file in files:
@@ -26,20 +28,35 @@ def clean_files(dir_path, test_mode=False):
             if test_mode:
                 print(f'Would delete: {file}')
             else:
-                dfiles+=1   # count deleted files
-                os.remove(file)  # delete file
+                dfiles += 1   	    # count deleted files
+                os.remove(file)     # delete file
 
         if test_mode:
             print(f'Would keep: {files[0][1]}')
     return dfiles
 
 
+def clean_dirs(dir_path, test_mode=False):
+    # Get list of directories in the path
+    directories = [os.path.join(dir_path, d) for d in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, d))]
+
+    # Sort directories by date (ascending order)
+    directories.sort()
+
+    # Keep the last directory and delete the rest
+    for directory in directories[:-1]:
+        if test_mode:
+            print(f'Would delete directory: {directory}')
+        else:
+            # Remove directory and all its contents recursively
+            shutil.rmtree(directory)
+
+    return len(directories[:-1])  # Return the number of deleted directories
 
 def getlastdir(dir_path):
     # Get the directories in dir_path
-    directories = [directory for directory in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, directory))]
-    # Compile the regular expression pattern
-    pattern = re.compile(r'.*\d+\.html')
+    directories = [directory for directory in os.listdir(dir_path)
+                   if os.path.isdir(os.path.join(dir_path, directory))]
     # Sort the directories based on their names
     sorted_directories = sorted(directories, reverse=True)
 
@@ -52,11 +69,8 @@ def getlastdir(dir_path):
     return latest_directory_path
 
 
-
 def ex_names(dir_path):
-
-    pattern = r'^(.*?)\d+\.html$'  # Regular expression pattern to match NAME NUMBER.html format
-
+    pattern = r'^(.*?)\d+\.html$'  # Regex pattern to match file
     filenames = []
     for filename in os.listdir(dir_path):
         if filename.endswith('.html'):
@@ -72,7 +86,8 @@ def ex_names(dir_path):
 #    print(filenames)
     return filenames
 
-def cat_files(dir_path, name, remove=True):
+
+def cat_files2(dir_path, name, remove=True):
     concatenated_files = 0  # Counter for concatenated files
     concatenated_content = ""
     for filename in os.listdir(dir_path):
@@ -82,11 +97,8 @@ def cat_files(dir_path, name, remove=True):
                 content = file.read()
                 concatenated_content += content
                 if remove:
-                    #print('remove ' + file_path)
                     os.remove(file_path)
-
             concatenated_files += 1
-
     output_file_path = dir_path + '/' + f"{name}.html"
     with open(output_file_path, 'w') as output_file:
         output_file.write(concatenated_content)
@@ -94,6 +106,30 @@ def cat_files(dir_path, name, remove=True):
 
     return concatenated_files
 
+
+def cat_files(dir_path, name, remove=True):
+    concatenated_files = 0  # Counter for concatenated files
+    concatenated_content = ""
+    for filename in os.listdir(dir_path):
+        if filename.startswith(name):
+            file_path = os.path.join(dir_path, filename)
+            with open(file_path, 'r') as file:
+                content = file.read()
+                sgirls = soup(content, 'html.parser')
+                girls = sgirls.find_all("div",
+                              {"class": "girl-list-item",
+                               "data-type": "listing"})
+                for girl in girls:
+                    concatenated_content += str(girl)
+                if remove:
+                    os.remove(file_path)
+            concatenated_files += 1
+    output_file_path = dir_path + '/' + f"{name}.html"
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(concatenated_content)
+        output_file.close()
+
+    return concatenated_files
 
 def check_file_exists(filename):
     try:
@@ -107,6 +143,7 @@ def check_file_exists(filename):
         print(f"The file {filename} does not exist!")
         return None
 
+
 def findhtmls(dir):
     htmls = []
     for file in os.listdir(dir):
@@ -116,6 +153,7 @@ def findhtmls(dir):
                 htmls.append(file)
     return htmls
 
+
 def count_occurrences(file, pattern):
     with open(file, 'r') as html_file:
         content = html_file.read()
@@ -124,66 +162,82 @@ def count_occurrences(file, pattern):
 
 
 def get_gals(dir_path, category, test=False):
-    dftmp = pd.DataFrame(columns=['Girl', 'Tel', 'Short',
-                               'Bezirk', 'Stadt', 'Strasse', 'Fans',
-                               'Gurl', 'Purl', 'a1', 'a0', 'cim', 'cof','t'])
+    left = '</strong>'
+    right = '<a'
+
     f = category
     fh = check_file_exists(dir_path+'/'+f)
-    if not fh: raise Exception('x', f)
+    if not fh:
+        raise Exception('x', f)
 
     # parses html into a soup data structure to traverse html
     # as if it were a json data type.
     # only input is content
-    a0,a1,cim,cof = '','','',''
-    if 'analsex' in category: a1='✓'
-    if 'natur' in category: a0='✓'
-    if 'cum_in_mouth' in category: cim='✓'
-    if 'cum_on_face' in category: cof='✓'
+    a0, a1, cim, cof = '', '', '', ''
+    if 'analsex' in category:
+        a1 = '✓'
+    if 'natur' in category:
+        a0 = '✓'
+    if 'cum_in_mouth' in category:
+        cim = '✓'
+    if 'cum_on_face' in category:
+        cof = '✓'
 
     tmp = []
     page_soup = soup(fh, "html.parser")
 
-    girls = page_soup.findAll("div", {"class": "girl-list-item", "data-type": "listing"})
+    girls = page_soup.findAll("div",
+                              {"class": "girl-list-item",
+                               "data-type": "listing"})
     print('   ✓ '+f+': '+str(len(girls)))
 
     for girl in girls:
-        # append the Girl in every loop
-        girl_name = girl.select_one("div a:nth-of-type(1)").text.strip()  	# girl name
+        girl_name = girl.find('h4').get_text(strip=True)
 
-        node = girl.div.div.select_one("div a:nth-of-type(1)")
-        if node is not None: stadt = node.text.strip()
-        else: stadt = 'Wien'
 
-        node = girl.div.div.select_one("div a:nth-of-type(2)")  	# bezirk
-        if node is not None: bezirk = node.text.strip()
-        else: bezirk = '1000'
+        location = girl.find("div", class_="g-location") # find the div element with class 'g-location'
+        hrefs = location.find_all("a")
+        stadt = hrefs[0].get_text() # get the text of the first element and assign it to stadt
+        bezirk = hrefs[1].get_text() # get the text of the second element and assign it to bezirk
+        try:
+            strasse = hrefs[2].get_text() # get the text of the third element and assign it to 
+        except:
+            strasse = ''
 
-        try: strasse = girl.div.div.select_one("div a:nth-of-type(3)").text
-        except: strasse = ''
-
-        try: fancount = girl.select_one("span[id*=girl-fancount]").text
-        except: fancount = 0
+        try:
+            fancount = girl.select_one("span[id*=girl-fancount]").text
+        except:
+            fancount = 0
 
         try:
             short_str = girl.find('div', class_='girl-subtitle')
 #            short = short_str[short_str.index(
-#                left)+len(left):short_str.index(right)].strip()
+#                              left)+len(left):short_str.index(right)].strip()
 #            print(short_str)
+#           short is the the part of the string after the first space
             short = short_str.get_text(strip=True, separator=' ')
-#            print(short)
+            # remove the first word
+            short = short[short.index(' ')+1:]
+
         except:
             short = ''
 #        exit()
 
         node = girl.find('a', {'class': 'pull-right'})
-        if node is not None: tel = node['href']
-        else: tel = '-'
+        if node is not None:
+            tel = node['href']
+        else:
+            tel = '-'
 
-        gurl = girl.find('a', href=True)['href']
+        #gurl = girl.find('a', href=True)['href']
+        h4 = girl.find("h4") # find the h4 element
+        gurl = h4.find("a")["href"] # find the a element inside the h4 element and get its 
 
         node = girl.find('source', srcset=True)
-        if node is not None: purl = node['srcset']
-        else: purl = None
+        if node is not None:
+            purl = node['srcset']
+        else:
+            purl = None
 
 
         tmp.append({'Girl': girl_name,
@@ -201,7 +255,6 @@ def get_gals(dir_path, category, test=False):
                     'cof': cof,                
                     't':''
                     })
-#    print (tmp)
     return tmp
 
 
@@ -232,6 +285,7 @@ def fancy_print(message, level=1):
     print(header)
     if level in [1, 2]: print(line)
 
+
 def someStats(df):
     # Number of rows that have all four columns checkmarked
     rows_all_checkmarks = df[(df['a1'] == '✓') & (df['a0'] == '✓') & (df['cof'] == '✓') & (df['cim'] == '✓')]
@@ -241,19 +295,91 @@ def someStats(df):
 
     # Print the statistics
     fancy_print("TOP10 Supergals                            ✓✓✓✓",level=2)
-    print(get_top_10_rows(rows_all_checkmarks,15))
+    print(get_top_10_rows(rows_all_checkmarks,5))
     fancy_print("TOP10 Ass                                  ✓✓??",level=2)
-    print(get_top_10_rows(rows_both_a1_a0))
+    print(get_top_10_rows(rows_both_a1_a0,5))
+    fancy_print("New Ass                                  ✓✓??",level=2)
+    print(get_top_10_rows(rows_both_a1_a0,5, Top=False))
     fancy_print("TOP10 Cum                                  ??✓✓",level=2)
-    print(get_top_10_rows(rows_both_cum))
+    print(get_top_10_rows(rows_both_cum,5))
     fancy_print("TOP10 A0Ass                                x✓??",level=2)
-    print(get_top_10_rows(rows_a0_only))
+    print(get_top_10_rows(rows_a0_only,5))
     
-def get_top_10_rows(top_10_rows, amount=10):
+def get_top_10_rows(top_10_rows, amount=10, Top=True):
     top_10_rows = top_10_rows.fillna('')  # Replace NaN values with empty string
     top_10_rows['Fans'] = top_10_rows['Fans'].astype(int)
-    top_10_rows = top_10_rows[['Girl', 'Strasse','Fans','a1','a0','cim','cof']].sort_values('Fans', ascending=False).head(amount)
+    top_10_rows = top_10_rows[['Girl', 'Strasse','Gurl','Fans','a1','a0','cim','cof']].sort_values('Fans', ascending = not Top).head(amount)
     top_10_rows = top_10_rows.reset_index(drop=True)  # Reset index and drop the original index column
     top_10_rows.index += 1  # Assign labels from 1 to 10
     top_10_rows.index.name = 'Rank'  # Add an index name
     return top_10_rows
+
+
+def convert_dataframe_to_html(df):
+    # Sort the DataFrame by index
+    df = df.sort_index()
+
+    # Add Img column with image tags
+    df.insert(0, 'Img', df['Purl'].apply(lambda x: f'<img src="{x}" style="max-width: 140px; max-height: 140px;">'))
+
+    # Convert Purl column to clickable image URLs
+    df['Purl'] = df['Purl'].apply(lambda x: f'<a href="{x}">img</a>')
+
+    # Convert Gurl column to clickable URLs
+    df['Gurl'] = df['Gurl'].apply(lambda x: f'<a href="{x}">url</a>')
+
+    # Replace NaN values with empty string
+    df = df.fillna("")
+
+    # Convert DataFrame to HTML table
+    table_html = df.to_html(escape=False, index=False, classes='sortable')
+
+    # Create complete HTML page
+    html = f'''
+    <html>
+    <head>
+        <style>
+            table {{
+                font-family: Arial, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+                border: none;
+            }}
+            
+            th, td {{
+                text-align: left;
+                padding: 8px;
+                border: none;
+            }}
+            
+            th.sortable {{
+                background-color: #007bff;
+                color: white;
+                cursor: pointer;
+            }}
+            
+            tr:nth-child(even) {{
+                background-color: #f2f2f2;
+            }}
+            
+            tr:hover {{
+                background-color: #e6e6ff;
+            }}
+            
+            img {{
+                max-width: 140px;
+                max-height: 140px;
+                border: none;
+            }}
+        </style>
+    </head>
+    <body>
+        {table_html}
+        <link href="https://cdn.jsdelivr.net/gh/tofsjonas/sortable@latest/sortable.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/gh/tofsjonas/sortable@latest/sortable.min.js"></script>
+
+    </body>
+    </html>
+    '''
+
+    return html
